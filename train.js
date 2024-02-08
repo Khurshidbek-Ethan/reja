68ae42b6c35f83d407c8a31f57afafd67118c47b
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-02-09 03:48:44 +0900

    feat: added new lines of code

--- train.js
+++ train.js
@@ -1,3 +1,8 @@
 // 2 parametrli function yozing 
 // hamda u functiondagi paramter 1 2 chi parameterdagi qatnashgan qandaydir sozni qaytarsin
-function countLetter(a,b){}
+function countLetter(a,b){
+    let count = 0
+    for(let i=0; i<b.length; i ++){
+        if(){}
+    }
+}
