0aa7d16d41d7be6c4e74903432be8451bccc3b46
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-02-14 18:48:09 +0900

    feat:edit va delete korib chiqdik

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // MongoDB chaqirish
 const db =require("./server").db();
-
+const mongodb = require("mongodb");
 
 // 1 Kirish code
 app.use(express.static("public"));
@@ -26,18 +26,25 @@ app.set("view engine","ejs");
 app.post("/create-item",(req,res) => {
     console.log('user entered/create-item');
 
-console.log(req.body);
-// res.end("success");
-const new_reja =req.body.reja;
-db.collection("plans").insertOne({reja: new_reja},(err,data)=>{
-    if(err){
-        console.log(err);
-        res.end('something went wrong');
-    }else {
-        res.end('successfully added');
-    }
+ // console.log(req.body);
+ // res.end("success");
+ const new_reja =req.body.reja;
+ db.collection("plans").insertOne({reja: new_reja},(err,data)=>{
+    console.log(data.ops);
+ res.json(data.ops[0]);
 });
 });
+
+app.post("/delete-item", (req,res)=>{
+const id= req.body.id;
+db.collection("plans").deleteOne(
+    {_id: new mongodb.ObjectId(id)},
+    function(err,data){
+    res.json({state:"success"});
+})
+
+});
+
 
 
 
