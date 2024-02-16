bf57d39da53267f7204ed7f89d87bc2c81a578d9
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-02-16 18:58:44 +0900

    feat:Update ni organib chiqdik

--- public/browser.js
+++ public/browser.js
@@ -66,8 +66,35 @@ if(e.target.classList.contains("delete-me")){
 //edit oper
 
 if(e.target.classList.contains("edit-me")){
-  alert("siz edit tugmasini bosdingiz");
+let userInput = prompt(
+  "Ozgartirish kiriting",
+   e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+   );
+if
+(userInput){
+axios
+.post("/edit-item",{
+  id: e.target.getAttribute("data-id"),
+  new_input:userInput,
+})
+.then((response)=>{
+console.log(response.data);
+e.target.parentElement.parentElement.querySelector(
+  ".item-text"
+  ).innerHTML = userInput;
+})
+.catch(err=>{
+  console.log("Iltimos qaytadan xarakat qiling!", );
+
+})
+}
 }
 
 });
 
+document.getElementById("clean-all").addEventListener("click",function(){
+  axios.post("/delete-all",{delete_all:true}).then(respose=>{
+    alert(respose.data.state);
+    document.location.reload();
+  })
+})
--- train.js
+++ train.js
@@ -1,74 +1,102 @@
-// Task D
-//Shunday class tuzing nomi Shop va uni constructoriga 3 hil mahsulot pass bolsin,
-// hamda classning 3 ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method
-// ishga tushgan vaqt ham log qilinsin.
-// Masalan: const shop = new Shop(4,5,2) shop.qoldiq() return hozir 20:40 4ta non, 5ta lagmon
-// va 2ta cola mavjud! shop.sotish("non", 3) & shop.qabul("cola", 4) & shop.qoldiq() return hozir 20:50 ta 1ta non va 5 ta lagmon va 6ta cola mavjud
-
-let now = new Date();
-let hour = now.getHours();
-let minute = now.getMinutes();
-minute = minute < 10 ? "0" + minute : minute;
-
-class Shop {
-  constructor(lagmon, non, cola) {
-    this.lagmon = lagmon;
-    this.non = non;
-    this.cola = cola;
+const checkContent = (a, b) => {
+  const arr = b.split("");
+  const new_arr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let count = 0;
+    if (a.length === arr.length && a.includes(arr[i])) {
+      count++;
+    }
+    new_arr.push(count);
   }
-
-  sotish(items, amount) {
-    // lagmon
-    if (items === "lagmon") {
-      if (this.lagmon >= amount) {
-        this.lagmon -= amount;
-      }
-    }
-
-    // non
-    if (items === "non") {
-      if (this.non >= amount) {
-        this.non -= amount;
-      }
-    }
-
-    // cola
-
-    if (items === "cola") {
-      if (this.cola >= amount) {
-        this.cola -= amount;
-      }
-    }
-
-    return `hozir ${hour}:${minute} da ${this.lagmon} ta lagmon, ${this.non} ta non va ${this.cola} ta cola bor holos!`;
+  if (new_arr.includes(0)) {
+    return false;
+  } else {
+    return true;
   }
+};
 
-  qabul(items, amount) {
-    // lagmon
-    if (items === "lagmon") {
-      this.lagmon += amount;
-    }
+const result = checkContent("mitgroup", "groupmti");
+console.log(result);
 
-    // non
-    if (items === "non") {
-      this.non += amount;
-    }
 
-    // cola
-    if (items === "cola") {
-      this.cola += amount;
-    }
-    return `hozir ${hour}:${minute} da ${this.lagmon} ta lagmon, ${this.non} ta non va ${this.cola} ta cola bor holos!`;
-  }
 
-  qoldiq() {
-    return `hozir ${hour}:${minute} da ${this.lagmon} ta lagmon, ${this.non} ta non va ${this.cola} ta cola qolgan!`;
-  }
-}
 
-const trade = new Shop(6, 2, 4);
-console.log(trade.sotish("cola", 2));
-console.log(trade.qabul("non", 2));
+
+
+
+
+
+// // Task D
+// //Shunday class tuzing nomi Shop va uni constructoriga 3 hil mahsulot pass bolsin,
+// // hamda classning 3 ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method
+// // ishga tushgan vaqt ham log qilinsin.
+// // Masalan: const shop = new Shop(4,5,2) shop.qoldiq() return hozir 20:40 4ta non, 5ta lagmon
+// // va 2ta cola mavjud! shop.sotish("non", 3) & shop.qabul("cola", 4) & shop.qoldiq() return hozir 20:50 ta 1ta non va 5 ta lagmon va 6ta cola mavjud
+
+// let now = new Date();
+// let hour = now.getHours();
+// let minute = now.getMinutes();
+// minute = minute < 10 ? "0" + minute : minute;
+
+// class Shop {
+//   constructor(lagmon, non, cola) {
+//     this.lagmon = lagmon;
+//     this.non = non;
+//     this.cola = cola;
+//   }
+
+//   sotish(items, amount) {
+//     // lagmon
+//     if (items === "lagmon") {
+//       if (this.lagmon >= amount) {
+//         this.lagmon -= amount;
+//       }
+//     }
+
+//     // non
+//     if (items === "non") {
+//       if (this.non >= amount) {
+//         this.non -= amount;
+//       }
+//     }
+
+//     // cola
+
+//     if (items === "cola") {
+//       if (this.cola >= amount) {
+//         this.cola -= amount;
+//       }
+//     }
+
+//     return `hozir ${hour}:${minute} da ${this.lagmon} ta lagmon, ${this.non} ta non va ${this.cola} ta cola bor holos!`;
+//   }
+
+//   qabul(items, amount) {
+//     // lagmon
+//     if (items === "lagmon") {
+//       this.lagmon += amount;
+//     }
+
+//     // non
+//     if (items === "non") {
+//       this.non += amount;
+//     }
+
+//     // cola
+//     if (items === "cola") {
+//       this.cola += amount;
+//     }
+//     return `hozir ${hour}:${minute} da ${this.lagmon} ta lagmon, ${this.non} ta non va ${this.cola} ta cola bor holos!`;
+//   }
+
+//   qoldiq() {
+//     return `hozir ${hour}:${minute} da ${this.lagmon} ta lagmon, ${this.non} ta non va ${this.cola} ta cola qolgan!`;
+//   }
+// }
+
+// const trade = new Shop(6, 2, 4);
+// console.log(trade.sotish("cola", 2));
+// console.log(trade.qabul("non", 2));
 
 
 
